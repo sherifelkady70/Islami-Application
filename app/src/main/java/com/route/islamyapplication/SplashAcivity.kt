03240581bf16a4f1b0b79899254c3ec028ebeee2
sherif Abdelrhman <sellkady038@gmail.com>
2024-01-21 23:34:17 +0200

    second Commit [ splash screen setup ]

---
+++ app/src/main/java/com/route/islamyapplication/SplashAcivity.kt
@@ -0,0 +1,27 @@
+package com.route.islamyapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashAcivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_acivity)
+
+        Handler().postDelayed(object : Runnable{
+            override fun run() {
+               startMainActivity()
+            }
+
+        },2000)
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this@SplashAcivity,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
