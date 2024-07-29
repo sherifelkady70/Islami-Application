974db0c94e87030b5d4cf820c158c8627403460b
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-29 17:32:22 +0300

    get list of radios in view model.

--- app/src/main/java/com/route/islamyapplication/radio/RadioViewModel.kt
+++ app/src/main/java/com/route/islamyapplication/radio/RadioViewModel.kt
@@ -1,7 +1,18 @@
 package com.route.islamyapplication.radio
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.route.islamyapplication.radio.api.RetrofitObject
+import com.route.islamyapplication.radio.api.models.Radio
+import kotlinx.coroutines.launch
 
 class RadioViewModel :ViewModel() {
+    var listOfRadio = MutableLiveData<List<Radio>?>()
 
+    fun loadRadios() {
+        viewModelScope.launch{
+            listOfRadio.value = RetrofitObject.getRetrofitInstance().getRadios().radios
+        }
+    }
 }
